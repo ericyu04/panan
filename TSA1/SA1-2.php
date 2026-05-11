72e75437c34dd164e8f3a83e6858ac0d3779824b
ericyu04 <ericsonjohn.yu24@gmail.com>
2026-05-11 18:02:59 +0800

    Create SA1-2.php

---
+++ TSA1/SA1-2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiplication Table</title>
+    <style>
+        .container{
+            display: grid;
+            grid-template-columns: repeat(11, 1fr);
+            aspect-ratio: 1 / 1;
+            border: 3px solid black;
+            margin: 20px;
+        }
+        .light {
+            background-color: #f0b13d;
+        }
+        .dark {
+            background-color: #9b7d2a;
+        }
+        .cell {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Multiplication Table</h1>
+    <div class="container">
+        <?php
+        for($row = 0; $row <= 10; $row++) {
+            for($col = 0; $col <= 10; $col++) {
+                $value = $row * $col;
+                $class = ($row + $col) % 2 == 0 ? 'light' : 'dark';
+                echo "<div class='cell $class'>$value</div>";
+            }
+        }
+        ?>
+    </div>
+</body>
+</html>
